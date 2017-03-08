fb1f0a636254b298867d1321cbeb0e8d50fee905
Unknown <wesley.leiter@gmail.com>
2017-03-08 00:09:13 -0800

    Got the passWord validation working

--- form-validation.js
+++ form-validation.js
@@ -17,9 +17,14 @@ function validate(){
   }
 
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if (passEntered == "password") {
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else {
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
