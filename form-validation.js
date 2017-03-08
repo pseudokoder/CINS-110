9419278e0766df2ab10e8932a7051eec8008b119
Unknown <wesley.leiter@gmail.com>
2017-03-08 00:01:19 -0800

    Got the userName verification working

--- form-validation.js
+++ form-validation.js
@@ -1,14 +1,20 @@
 function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
+  var userLength = userEntered.length;
   var passEntered = document.getElementById("pass").value;
 
   //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  if (userLength < 6) {
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  else {
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
 
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
