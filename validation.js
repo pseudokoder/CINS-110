5099302af660c872f9c0ad60f6cbaaa0a671f1ca
Unknown <wesley.leiter@gmail.com>
2017-04-01 02:18:17 -0700

    completed individual pages and developed results page

--- validation.js
+++ validation.js
@@ -1,67 +1,47 @@
 // Author: Wes Leiter
 
-function validateUsername(){
+function validateAns(){
   //Grab the user's input and store in variables
-  var userEntered = document.getElementById("user").value;
-  var userLength = userEntered.length;
-  var userSpace = userEntered.search(" ");
-  //var passEntered = document.getElementById("pass").value;
-  //var passLength = passEntered.length;
+  var ansEntered = document.getElementById("answer").value;
+  var ansLength = ansEntered.length;
+  var ansLower = ansEntered.toLowerCase();
+  console.log("ansEntered: " + ansEntered);
+  console.log("ansLength: " + ansLength);
+  console.log("ansLower: " + ansLower);
 
   //Show message that there is an error with the username...
-  if (userLength < 6) {
-    document.getElementById("usernameError").innerHTML="Username must have at least 6 characters.";
-    document.getElementById("usernameError").classList.remove("hidden-message");
-    document.getElementById("usernameError").classList.add("shown-message");
+  if (ansLength == 0){
+    document.getElementById("ansError").classList.remove("shown-message");
+    document.getElementById("ansError").classList.add("hidden-message");
+    document.getElementById("ansGroup").classList.remove("has-error");
+    document.getElementById("ansGroup").classList.remove("has-success");
+  }
+  else if (ansLength > 1) {
+    document.getElementById("ansError").innerHTML="Enter only 1 letter.";
+    document.getElementById("ansError").classList.remove("hidden-message");
+    document.getElementById("ansError").classList.add("shown-message");
     //Turn the username items red
-    document.getElementById("usernameGroup").classList.remove("has-success");
-    document.getElementById("usernameGroup").classList.add("has-error");
+    document.getElementById("ansGroup").classList.remove("has-success");
+    document.getElementById("ansGroup").classList.add("has-error");
   }
-  else if (userSpace != -1) {
-    document.getElementById("usernameError").innerHTML="Username must NOT contain any spaces.";
-    document.getElementById("usernameError").classList.remove("hidden-message");
-    document.getElementById("usernameError").classList.add("shown-message");
-    //Turn the username items red
-    document.getElementById("usernameGroup").classList.remove("has-success");
-    document.getElementById("usernameGroup").classList.add("has-error");
-  }
-  else {
-    document.getElementById("usernameError").classList.remove("shown-message");
-    document.getElementById("usernameError").classList.add("hidden-message");
-    document.getElementById("usernameGroup").classList.remove("has-error");
-    document.getElementById("usernameGroup").classList.add("has-success");
-  }
-}
-
-function validatePassword(){
-  //Show message that there is an error with the password...
-  var userEntered = document.getElementById("user").value;
-  var passEntered = document.getElementById("pass").value;
-  var passLength = passEntered.length;
-  var passLower = passEntered.toLowerCase();
-
-  if (passLower == "password" || passEntered == userEntered)
+  else if (ansLower != "a" &&
+           ansLower != "b" &&
+           ansLower != "c" &&
+           ansLower != "d" &&
+           ansLower != "e")
   {
-    document.getElementById("passwordError").innerHTML="Password must NOT be the word \"password\" or be the same as the Username.";
-    document.getElementById("passwordError").classList.remove("hidden-message");
-    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("ansError").innerHTML="Answer must be a letter A through E";
+    document.getElementById("ansError").classList.remove("hidden-message");
+    document.getElementById("ansError").classList.add("shown-message");
     //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
-    document.getElementById("passwordGroup").classList.add("has-error");
-  }
-  else if (passLength < 6 || passLength > 20) {
-    document.getElementById("passwordError").innerHTML="Password must be between 6-20 characters.";
-    document.getElementById("passwordError").classList.remove("hidden-message");
-    document.getElementById("passwordError").classList.add("shown-message");
-    //Turn the password items red
-    document.getElementById("passwordGroup").classList.remove("has-success");
-    document.getElementById("passwordGroup").classList.add("has-error");
+    document.getElementById("ansGroup").classList.remove("has-success");
+    document.getElementById("ansGroup").classList.add("has-error");
   }
   else {
-    document.getElementById("passwordError").classList.remove("shown-message");
-    document.getElementById("passwordError").classList.add("hidden-message");
-    document.getElementById("passwordGroup").classList.remove("has-error");
-    document.getElementById("passwordGroup").classList.add("has-success");
+    document.getElementById("ansError").classList.remove("shown-message");
+    document.getElementById("ansError").classList.add("hidden-message");
+    document.getElementById("ansGroup").classList.remove("has-error");
+    document.getElementById("ansGroup").classList.add("has-success");
   }
 }
 
@@ -69,8 +49,18 @@ function begin(){
   window.location.href="test1.html";
 }
 
-function register(){
-  var userEntered = document.getElementById("user").value;
-  var passEntered = document.getElementById("pass").value;
-  window.confirm("Username: " + userEntered + "\nPassword: " + passEntered);
+function submit1(){
+  var ansEntered = document.getElementById("answer").value;
+  window.confirm("You answered: " + ansEntered);
+  window.location.href="test2.html";
+}
+function submit2(){
+  var ansEntered = document.getElementById("answer").value;
+  window.confirm("You answered: " + ansEntered);
+  window.location.href="test3.html";
+}
+function submit3(){
+  var ansEntered = document.getElementById("answer").value;
+  window.confirm("You answered: " + ansEntered);
+  window.location.href="results.html";
 }
