3da2689e2ec405b7b060f908a0ae62141974aa2f
Unknown <wesley.leiter@gmail.com>
2017-04-17 21:12:42 -0700

    initial setup

---
+++ datalogger.js
@@ -0,0 +1,9 @@
+var numberValues;
+numberValues =
+
+function add() {
+  // get the text input from the DOM and store it in a variable
+  // check if that variable is a number
+  // add it to the array.push
+
+}
