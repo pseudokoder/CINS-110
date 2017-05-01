0b0f324cbc9c455503ada10aea4d39aaf573e647
Unknown <wesley.leiter@gmail.com>
2017-05-01 12:27:18 -0700

    added some stuff

--- sales.js
+++ sales.js
@@ -4,16 +4,17 @@ function addItem()
 {
   var newItem;
   newItem = document.getElementById("price").value;
-  //IF newItem is not a number
-  // THEN show an alert: "Enter price as a number"
-  //OTHERWISE,
-  // update newItem to its value cast as a number
-  // update runningTotal to be its value plus newItem
-  // create a variable called dollars
-  // call asCurrency() by with the value of runningTotal and assign the return value to dollars
-  // update the innerHTML of the span with the id "subtotal" to be dollars
-  // update the value of the input with the id "price" to be an empty string
-  // update a cookie called "preTax" with the value of runningTotal
+  if (isNaN(newItem)) {  //IF newItem is not a number
+    alert("Enter price as a number"); // THEN show an alert
+  }
+  else {
+    newItem = Number(newItem); // update newItem to its value cast as a number
+    runningTotal += newItem; // update runningTotal to be its value plus newItem
+    var dollars = asCurrency(runningTotal); // call asCurrency() by with the value of runningTotal and assign the return value to dollars
+    document.getElementById("subtotal").innerHTML = dollars; // update the innerHTML of the span with the id "subtotal" to be dollars
+    // update the value of the input with the id "price" to be an empty string
+    // update a cookie called "preTax" with the value of runningTotal
+  }
 }
 
 //takes a number and gives a string with the number displayed as USD currency
