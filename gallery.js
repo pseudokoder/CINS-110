908fb7e80ca4d1c9ea0f59623ec07de718385d96
Unknown <wesley.leiter@gmail.com>
2017-05-06 00:48:37 -0700

    done

--- gallery.js
+++ gallery.js
@@ -1,6 +1,13 @@
+var chico = ["Bidwell Mansion","Bidwell Park","Titan Missile Silo",
+             "Esplanade Blvd","Sierra Nevada Brewing Co"];
+var current = 0;
 
 
 function shuffleGallery()
 {
-
+  do {
+    var i = Math.floor(Math.random() * 5); // generates random intiger between 0 and 4
+  } while (i == current);
+  current = i;
+  return i;
 }
