62e6f6c9788fb268fbedcecd7e16c170f670bfb0
Unknown <wesley.leiter@gmail.com>
2017-05-06 01:29:23 -0700

    created array of objects

--- gallery.js
+++ gallery.js
@@ -1,7 +1,27 @@
-var chico = ["Bidwell Mansion","Bidwell Park","Titan Missile Silo",
-             "Esplanade Blvd","Sierra Nevada Brewing Co"];
+// Wes Leiter
+var chico = [
+  { description:"Chico State Campus",
+    picture:"http://www.csuchico.edu/maps/images/photo-tour/phototour-kendall-768.jpg",
+    from:"csuchico.edu"
+  },
+  { description:"Bidwell Park",
+    picture:"http://1.bp.blogspot.com/-iQmg1olEo8E/UolfG6zFwBI/AAAAAAAAI1k/72tpB2V4xNI/s1600/2013_1111_044.jpg",
+    from:"adunnphotography.blogspot.com"
+  },
+  { description:"Titan Missile Silo",
+    picture:"https://tylerashresume.files.wordpress.com/2012/11/chico-missile-silos.jpg",
+    from:"tylerashresume.files.wordpress.com"
+  },
+  { description:"Esplanade Blvd",
+    picture:"https://photos.smugmug.com/News-Week-of-12-5-2016/i-6KMHqBS/0/XL/Esplanade%24%24EB8-XL.jpg",
+    from:"media.chicoer.com"
+  },
+  { description:"Sierra Nevada Brewing Co",
+    picture:"http://brewpublic.com/wp-content/uploads/2008/12/roadtrip-petaluma-to-sf-and-back-08-369.jpg",
+    from:"brewpublic.com"
+  },
+];
 var current = 0;
-
 
 function shuffleGallery()
 {
